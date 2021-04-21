fa9f39418f26e7733e79d7a128185d4a6c76a6e9
Sergey Setkov <s.setkov@gmail.com>
2021-04-21 19:07:24 +0300

    move template to embeded FS

--- WebUI/MessageQueueController.go
+++ WebUI/MessageQueueController.go
@@ -2,17 +2,21 @@ package WebUI
 
 import (
 	"BugNetSyncService/Common"
+	"embed"
 	"fmt"
 	"html/template"
 	"net/http"
 )
+
+//go:embed Static
+var static embed.FS
 
 func (webUI *WebUI) MessageQueueController(w http.ResponseWriter, r *http.Request) {
 	messageQueue, err := webUI.dataService.GetMessageQueue(100)
 	if err != nil {
 		fmt.Fprint(w, err)
 	} else {
-		tmpl, err := template.New("MessageQueueView.html").Funcs(webUI.funcMap).ParseFiles("WebUI/MessageQueueView.html")
+		tmpl, err := template.New("MessageQueueView.html").Funcs(webUI.funcMap).ParseFS(static, "Static/MessageQueueView.html")
 		if err != nil {
 			fmt.Fprint(w, Common.NewError("Parse temptate MessageQueueView. "+err.Error()))
 		} else {
